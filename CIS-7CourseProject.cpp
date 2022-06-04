438da08ae7d21b566ca128a2b620b0b2b2d80dd2
RitterSegen <90066652+RitterSegen@users.noreply.github.com>
2022-06-04 00:32:50 -0700

    Update CIS-7CourseProject.cpp

--- CIS-7CourseProject.cpp
+++ CIS-7CourseProject.cpp
@@ -9,6 +9,25 @@ Choice 4: Map/Matrix (DONE)
 */
 #include <bits/stdc++.h>
 using namespace std;
+
+//A utility function to add an edge in
+//an undirected graph
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printGraphChoice2(vector<int> adj[], int V)
+{
+    for (int v = 1; v < V; ++v)
+    {
+        cout << "\n Most cost effective path " << v;
+        for (auto x: adj[v])
+           cout << " -> " << x;
+        printf("\n");
+    }
+}
 // Data structure to store Adjacency list nodes
 struct Node {
     int val, cost;
